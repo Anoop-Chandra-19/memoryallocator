9e6dece80fbfd7743236cbaaf5c8a53e26ce1e90
Anoop <anoopchandraparampalli@gmail.com>
2025-03-28 17:14:56 -0400

    basic implementation of malloc

---
+++ memalloc.c
@@ -0,0 +1,18 @@
+// This is a simple implementation of malloc using sbrk.
+// It is not thread-safe and should not be used in production code.
+#include <unistd.h> // For sbrk
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <stddef.h>
+void *malloc(size_t size)
+{
+    void *block;
+    block = sbrk(size);
+    if (block == (void *)-1)
+    {
+        return NULL; // Allocation failed//-
+        return NULL;//+
+    }
+    return block; // Return the pointer to the allocated memory//-
+    return block;//+
+}
